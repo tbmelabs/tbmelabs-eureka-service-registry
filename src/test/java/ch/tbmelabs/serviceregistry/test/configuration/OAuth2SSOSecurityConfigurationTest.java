4c903f05cdaa1db4b25576977d1ff8414c4da7c9
bbortt <timon.borter@post.ch>
2018-10-23 22:37:43 +0200

    first attempts with keycloak.. however there seems to exist a circular dependency.

--- src/test/java/ch/tbmelabs/serviceregistry/test/configuration/OAuth2SSOSecurityConfigurationTest.java
+++ src/test/java/ch/tbmelabs/serviceregistry/test/configuration/OAuth2SSOSecurityConfigurationTest.java
@@ -1,28 +1,26 @@
 package ch.tbmelabs.serviceregistry.test.configuration;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
-import ch.tbmelabs.serviceregistry.configuration.OAuth2SSOSecurityConfiguration;
 import org.junit.Test;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import ch.tbmelabs.serviceregistry.configuration.OAuth2SSOSecurityConfiguration;
 
 public class OAuth2SSOSecurityConfigurationTest {
 
   @Test
   public void shouldBeAnnotated() {
     assertThat(OAuth2SSOSecurityConfiguration.class).hasAnnotation(Configuration.class)
-      .hasAnnotation(EnableOAuth2Sso.class).hasAnnotation(EnableWebSecurity.class)
-      .hasAnnotation(EnableGlobalMethodSecurity.class);
+        // .hasAnnotation(EnableOAuth2Sso.class)
+        .hasAnnotation(EnableWebSecurity.class).hasAnnotation(EnableGlobalMethodSecurity.class);
   }
 
   @Test
   public void enablesGlobalPrePostMethodSecurity() {
     final EnableGlobalMethodSecurity enableGlobalMethodSecurityAnnotation =
-      OAuth2SSOSecurityConfiguration.class
-        .getDeclaredAnnotation(EnableGlobalMethodSecurity.class);
+        OAuth2SSOSecurityConfiguration.class
+            .getDeclaredAnnotation(EnableGlobalMethodSecurity.class);
 
     assertThat(enableGlobalMethodSecurityAnnotation.prePostEnabled()).isTrue();
   }
